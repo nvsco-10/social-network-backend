9f615d671b919cadda60121f7a216d172cb5203d
Neema Velasco <neemavelasco@gmail.com>
2022-04-22 18:55:38 -0500

    add delete and add friend in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,4 +59,36 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+
+  // add new friend to user's friend list
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // delete friend from friend list
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: { friendId: req.params.friendId } } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
